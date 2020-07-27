259b3a6e22f9d5b4a3c78bff812b2815756d8739
raymondlow <cocker123>
2020-07-28 03:43:14 +0800

    Remove secret key

--- plug_mate/settings.py
+++ plug_mate/settings.py
@@ -24,7 +24,7 @@ TEMPLATE_DIR = [os.path.join(BASE_DIR, 'control_interface/build'), os.path.join(
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 # EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-SECRET_KEY = '$4r^m&ig1mduh97%0mt@96v*(eztm%$l54(hk36)j5e126a8up'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
