df2910451801fd7a7c1d5eaecfef1cb96757cf1d
loocurse <41948452+loocurse@users.noreply.github.com>
2020-07-29 22:10:34 +0800

    added build folder

---
+++ control_interface/build/precache-manifest.fd969a726f36be5ddafcb93b2b239257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37941a622f16f364c8f86f2421eb8685",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0812e6a62bf516e1f42e",
+    "url": "/static/css/2.8a8576c8.chunk.css"
+  },
+  {
+    "revision": "5c2ee3ad8b0b0284ff93",
+    "url": "/static/css/main.fa06bb2a.chunk.css"
+  },
+  {
+    "revision": "0812e6a62bf516e1f42e",
+    "url": "/static/js/2.3d4442c5.chunk.js"
+  },
+  {
+    "revision": "5c2ee3ad8b0b0284ff93",
+    "url": "/static/js/main.b1442da6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
