d8ce8e52f25adcd6025a4b62ecf7e5130cc62f1b
mirabeltukiman <56396669+mirabeltukiman@users.noreply.github.com>
2020-08-06 17:27:37 +0800

    Update serializers.py

--- plug_mate_app/serializers.py
+++ plug_mate_app/serializers.py
@@ -1,20 +1,19 @@
 from rest_framework import serializers
-# from .models import RemoteControl, ScheduleBasedControl, PresenceBasedControl
-#
-#
-# class RemoteSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = RemoteControl
-#         fields = '__all__'
-#
-#
-# class ScheduleSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = ScheduleBasedControl
-#         fields = '__all__'
-#
-#
-# class PresenceSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = PresenceBasedControl
-#         fields = '__all__'
+from .models import RemoteData, ScheduleData, PresenceData
+
+class RemoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RemoteData
+        fields = ('id', 'user_id', 'device_type', 'device_state')
+
+class ScheduleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScheduleData
+        fields = ('id', 'user_id', 'event_id', 'event_start', 'event_end', 'event_name', 'event_rrule', 'device_type_id', 'device_type')
+
+class PresenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PresenceData
+        fields = ('id', 'user_id', 'device_type', 'presence_setting')
+
+
