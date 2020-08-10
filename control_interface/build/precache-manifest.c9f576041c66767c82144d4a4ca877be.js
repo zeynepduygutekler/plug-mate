1e93ee4d321371f21ff5f0ce120841d6427ad8af
mirabeltukiman <56396669+mirabeltukiman@users.noreply.github.com>
2020-08-10 15:20:53 +0800

    Fixed side navigation to follow scroll

--- control_interface/build/precache-manifest.c9f576041c66767c82144d4a4ca877be.js
+++ control_interface/build/precache-manifest.c9f576041c66767c82144d4a4ca877be.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "290203ae351f4720e16ac2a8084853a0",
+    "revision": "53c73eb06e374544a862ec9b77d2ba6f",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.d8a330f6.chunk.css"
   },
   {
-    "revision": "f2464f9e3c1c7418f1ff",
-    "url": "/static/css/main.2d719a46.chunk.css"
+    "revision": "8c4be483a69aefd6826a",
+    "url": "/static/css/main.22d49633.chunk.css"
   },
   {
     "revision": "87709b222393e8d5d526",
     "url": "/static/js/2.11ab0834.chunk.js"
   },
   {
-    "revision": "f2464f9e3c1c7418f1ff",
-    "url": "/static/js/main.b7f2636c.chunk.js"
+    "revision": "8c4be483a69aefd6826a",
+    "url": "/static/js/main.d58a7fc0.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- control_interface/build/service-worker.js
+++ control_interface/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4bc7b8dcb012cf599f8a4290de53d63c.js"
+  "/precache-manifest.c9f576041c66767c82144d4a4ca877be.js"
 );
 
 self.addEventListener('message', (event) => {
