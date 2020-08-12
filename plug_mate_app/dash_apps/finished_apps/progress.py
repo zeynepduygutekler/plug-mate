b4b303da8d130d83f2024506acd732d30139ab43
Lucas Ng <lucas_ng@mymail.sutd.edu.sg>
2020-08-12 13:26:27 +0800

    achievement table reads off from db

--- plug_mate_app/dash_apps/finished_apps/progress.py
+++ plug_mate_app/dash_apps/finished_apps/progress.py
@@ -17,44 +17,44 @@ from datetime import datetime
 
 def get_achievements():
     """Reads achievement dataframes from database"""
-
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM achievements_daily WHERE user_id=%s", [1])
-        results = cursor.fetchall()
-        colnames = [desc[0] for desc in cursor.description]
-    daily = pd.DataFrame(results, columns=colnames)
-    daily.drop(columns=['user_id'], inplace=True)
-    daily = daily.set_index('week_day')
-
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM achievements_weekly WHERE user_id=%s", [1])
-        results = cursor.fetchall()
-        colnames = [desc[0] for desc in cursor.description]
-    weekly = pd.DataFrame(results, columns=colnames)
-    weekly.drop(columns=['user_id'], inplace=True)
-
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM achievements_bonus WHERE user_id=%s", [1])
-        results = cursor.fetchall()
-        colnames = [desc[0] for desc in cursor.description]
-    bonus = pd.DataFrame(results, columns=colnames)
-    bonus.drop(columns=['user_id', 'id'], inplace=True)
-
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM achievements_points")
-        results = cursor.fetchall()
-        colnames = [desc[0] for desc in cursor.description]
-    reference = pd.DataFrame(results, columns=colnames)
-    reference = reference.set_index('achievement')
-    # daily = pd.read_csv('plug_mate_app/dash_apps/finished_apps/tables_csv/achievements_daily.csv')
+    #
+    # with connection.cursor() as cursor:
+    #     cursor.execute("SELECT * FROM achievements_daily WHERE user_id=%s", [1])
+    #     results = cursor.fetchall()
+    #     colnames = [desc[0] for desc in cursor.description]
+    # daily = pd.DataFrame(results, columns=colnames)
     # daily.drop(columns=['user_id'], inplace=True)
     # daily = daily.set_index('week_day')
-    # weekly = pd.read_csv('plug_mate_app/dash_apps/finished_apps/tables_csv/achievements_weekly.csv')
+    #
+    # with connection.cursor() as cursor:
+    #     cursor.execute("SELECT * FROM achievements_weekly WHERE user_id=%s", [1])
+    #     results = cursor.fetchall()
+    #     colnames = [desc[0] for desc in cursor.description]
+    # weekly = pd.DataFrame(results, columns=colnames)
     # weekly.drop(columns=['user_id'], inplace=True)
-    # bonus = pd.read_csv('plug_mate_app/dash_apps/finished_apps/tables_csv/achievements_bonus.csv')
-    # bonus.drop(columns=['user_id', 'id', 'cum_savings'], inplace=True)
-    # reference = pd.read_csv('plug_mate_app/dash_apps/finished_apps/tables_csv/achievements_points.csv')
+    #
+    # with connection.cursor() as cursor:
+    #     cursor.execute("SELECT * FROM achievements_bonus WHERE user_id=%s", [1])
+    #     results = cursor.fetchall()
+    #     colnames = [desc[0] for desc in cursor.description]
+    # bonus = pd.DataFrame(results, columns=colnames)
+    # bonus.drop(columns=['user_id', 'id'], inplace=True)
+    #
+    # with connection.cursor() as cursor:
+    #     cursor.execute("SELECT * FROM achievements_points")
+    #     results = cursor.fetchall()
+    #     colnames = [desc[0] for desc in cursor.description]
+    # reference = pd.DataFrame(results, columns=colnames)
     # reference = reference.set_index('achievement')
+    daily = pd.read_csv('plug_mate_app/dash_apps/finished_apps/tables_csv/achievements_daily.csv')
+    daily.drop(columns=['user_id'], inplace=True)
+    daily = daily.set_index('week_day')
+    weekly = pd.read_csv('plug_mate_app/dash_apps/finished_apps/tables_csv/achievements_weekly.csv')
+    weekly.drop(columns=['user_id'], inplace=True)
+    bonus = pd.read_csv('plug_mate_app/dash_apps/finished_apps/tables_csv/achievements_bonus.csv')
+    bonus.drop(columns=['user_id', 'id', 'cum_savings'], inplace=True)
+    reference = pd.read_csv('plug_mate_app/dash_apps/finished_apps/tables_csv/achievements_points.csv')
+    reference = reference.set_index('achievement')
 
     return daily, weekly, bonus, reference
 
@@ -102,38 +102,9 @@ app.layout = html.Div([
 
     html.Div([
         dcc.Interval(id='interval', interval=800000, n_intervals=0, max_intervals=1),
-        html.Table(id='achievements',className='table', children=[
-                       html.Tr([html.Th('Daily Achievements'), html.Th("Energy Points")],
-                               style={'background-color': '#1cc88a', 'color': 'white'})
-                   ] + [
-                       html.Tr([html.Td('Clock a higher cost savings than yesterday'), html.Td('20 points')]),
-                       html.Tr([html.Td('Turn off your plug loads using the remote feature', style={'opacity': 0.3}),
-                                html.Td(html.Img(src='https://i.ibb.co/qJqjkk8/trophy.png',
-                                                 style={'height': '6%'}))]),
-                       html.Tr([html.Td('Turn off your plug loads during lunch'), html.Td(html.P('40 points'))]),
-                       html.Tr([html.Td('Complete all daily achievements'), html.Td('100 points')])
-                   ] + [
-                       html.Tr([html.Th('Weekly Achievements'), html.Th("Points")],
-                               style={'background-color': '#4e73df', 'color': 'white'})
-                   ] + [
-                       html.Tr([html.Td('Clock a higher cost savings than last week'), html.Td('100 points')]),
-                       html.Tr([html.Td("Set next week's schedule-based controls"), html.Td('100 points')]),
-                       html.Tr([html.Td("Complete all weekly achievements"), html.Td('300 points')])
-                   ] + [
-                       html.Tr([html.Th('Bonus Achievements'), html.Th("Points")],
-                               style={'background-color': '#6f42c1', 'color': 'white'})
-                   ] + [
-                       html.Tr([html.Td('Save your first tree', style={'opacity': 0.3}), html.Td(
-                           html.Img(src='https://i.ibb.co/qJqjkk8/trophy.png',
-                                    style={'height': '6%'}))]),
-                       html.Tr([html.Td('Try out our simulation feature'), html.Td('100 points')], ),
-                       html.Tr([html.Td('Set your first schedule-based setting', style={'opacity': 0.3}),
-                                html.Td(html.Img(src='https://i.ibb.co/qJqjkk8/trophy.png',
-                                                 style={'height': '6%'}))]),
-                       html.Tr([html.Td('Set your first presence-based setting'), html.Td('100 points')]),
-                       html.Tr([html.Td('Complete all achievements'), html.Td('500 points')]),
+        html.Table(id='achievements',className='table',
 
-                   ])
+                   )
     ], style={"maxHeight": "19rem", "overflow": "scroll"})
 
 ], style={'display': 'inline-block', 'vertical-align': 'middle'})
@@ -147,14 +118,17 @@ app.layout = html.Div([
 def update_achievements_table(n):
     daily, weekly, bonus, reference = get_achievements()
     today = datetime.today().strftime('%a')
+    print(weekly)
     table = []
     # print(daily)
 
     def create_table_row(achievement, points):
         if points > 0:
-            return html.Tr([html.Td(reference.loc[achievement]['description'], style={'opacity': 0.3}),
-                     html.Td(html.Img(src='https://i.ibb.co/qJqjkk8/trophy.png',
-                                      style={'height': '6%'}))]),
+            # TODO fix bug here
+            return html.Tr([
+                html.Td(reference.loc[achievement]['description'], style={'opacity': 0.3}),
+                html.Td(html.Img(src='https://i.ibb.co/qJqjkk8/trophy.png',
+                                 style={'height': '6%'}))]),
         else:
             return html.Tr([html.Td(reference.loc[achievement]['description']),
                      html.Td(f'{reference.loc[achievement]["points"]} points')])
@@ -166,18 +140,17 @@ def update_achievements_table(n):
     bonus_header = [html.Tr([html.Th('Bonus Achievements'), html.Th("Points")],
                             style={'background-color': '#6f42c1', 'color': 'white'})]
     table += daily_header
-    # print(daily.loc[today].to_dict().items())
-    # print(daily.loc[today])
     for achmt,pts in daily.loc[today].to_dict().items():
         table += [create_table_row(achmt,pts)]
     table += weekly_header
     for achmt, pts in weekly.iloc[0].to_dict().items():
-        print(achmt, pts)
+        # print(achmt, pts)
         table += [create_table_row(achmt, pts)]
     table += bonus_header
     for achmt, pts in bonus.iloc[0].to_dict().items():
+        # print(achmt, pts)
         table += [create_table_row(achmt, pts)]
 
-
+    # print(table)
     return table, ''
 
