f321371888dcb979a71114b34ad22b167aa618fc
mirabeltukiman <56396669+mirabeltukiman@users.noreply.github.com>
2020-08-12 17:35:31 +0800

    Updated presence-based control

---
+++ static/precache-manifest.30f6977a7402ddf02f6e0029602e68f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "431d1ffa511352526602e5eec0209bac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "352961186952fac2b5bf",
+    "url": "/static/css/2.cfa39ebd.chunk.css"
+  },
+  {
+    "revision": "fd281e184bf70aad1b27",
+    "url": "/static/css/main.170baf40.chunk.css"
+  },
+  {
+    "revision": "352961186952fac2b5bf",
+    "url": "/static/js/2.4332d772.chunk.js"
+  },
+  {
+    "revision": "fd281e184bf70aad1b27",
+    "url": "/static/js/main.fa1a2264.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
