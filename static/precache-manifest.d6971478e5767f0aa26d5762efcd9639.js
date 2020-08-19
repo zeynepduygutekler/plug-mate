4304e965fba6c7e1c40eba4b1442e824ba4c34bb
mirabeltukiman <56396669+mirabeltukiman@users.noreply.github.com>
2020-08-19 17:46:24 +0800

    Added notification for first remote achievement

---
+++ static/precache-manifest.d6971478e5767f0aa26d5762efcd9639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63fa81927599d0465bf5f40ee39f4e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99f70b6d6c954cc3c296",
+    "url": "/static/css/2.bf8e2397.chunk.css"
+  },
+  {
+    "revision": "6cd3e74d6d9257b862c5",
+    "url": "/static/css/main.ec071929.chunk.css"
+  },
+  {
+    "revision": "99f70b6d6c954cc3c296",
+    "url": "/static/js/2.073cdfd9.chunk.js"
+  },
+  {
+    "revision": "6cd3e74d6d9257b862c5",
+    "url": "/static/js/main.4d395ec5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
